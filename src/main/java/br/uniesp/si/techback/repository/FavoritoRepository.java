1de8ad62d801ee426136de0d4a46d3520eaba1c4
Lucas Aguiar <llucasaguiar16@gmail.com>
2026-05-04 22:01:41 -0300

    Add files via upload

--- src/main/java/br/uniesp/si/techback/repository/FavoritoRepository.java
+++ src/main/java/br/uniesp/si/techback/repository/FavoritoRepository.java
@@ -2,8 +2,15 @@ package br.uniesp.si.techback.repository;
 
 import br.uniesp.si.techback.model.Favorito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FavoritoRepository extends JpaRepository<Favorito, Long> {
+
+    @Query("select f from Favorito f order by f.titulo")
+    public List<Favorito> listarFilmesFavoritos();
+
 }
--- src/main/java/br/uniesp/si/techback/repository/FilmeRepository.java
+++ src/main/java/br/uniesp/si/techback/repository/FilmeRepository.java
@@ -2,9 +2,25 @@ package br.uniesp.si.techback.repository;
 
 import br.uniesp.si.techback.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+
+
+    @Query("select f from Filme f order by f.titulo asc")
+    public List<Filme> listarFilmeAsc();
+
+    public List<Filme> findAllByOrderTituloAsc();
+
+    Optional<Filme> findByGenero(String genero);
+
+    @Query("select f from Filme f where f.genero = :genero ")
+    Filme buscarPorGenero(@Param("genero") String genero);
 
 }
